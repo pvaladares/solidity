a994b2f69f6bc69f960963347cc6b811cec0fed2
PV <2190926+pvaladares@users.noreply.github.com>
2022-04-22 17:38:06 +0100

    Added optional functions ERC20

--- Udemy2022/Project 4 -  Implementing an ERC20 Token/Token.sol
+++ Udemy2022/Project 4 -  Implementing an ERC20 Token/Token.sol
@@ -14,12 +14,12 @@ interface ERC20Interface {
     function transfer(address to, uint tokens) external returns (bool success);
     
     // Optionals
-    // function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    // function approve(address spender, uint tokens) external returns (bool success);
-    // function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
     
     event Transfer(address indexed from, address indexed to, uint tokens);
-    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
 
@@ -34,6 +34,8 @@ contract Cryptos is ERC20Interface{
     mapping(address => uint) public balances;
     // balances[0x1111...] = 100;
     
+    mapping(address => mapping(address => uint)) public allowed;
+
     constructor(){
         totalSupply = 1e6;
         founder = msg.sender;
@@ -48,7 +50,32 @@ contract Cryptos is ERC20Interface{
         require(balances[msg.sender] >= tokens, "Not enough tokens on the wallet!");
         balances[to] += tokens;
         balances[msg.sender] -= tokens;
+
         emit Transfer(msg.sender, to, tokens);
-        return true; // on
+
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining){
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public override returns (bool success){
+        require(balanceOf(msg.sender) >= tokens);
+        require(tokens > 0);
+
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+        require(allowed[from][to] >= tokens);
+        require(balances[from] >= tokens);
+
+        balances[from] -= tokens;
+        balances[to] += tokens;
+        allowed[from][to] -= tokens;
+        return true;
     }
 }
