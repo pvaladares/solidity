04a9b48106eb7cac9266a2470e07f98c4e487d65
PV <2190926+pvaladares@users.noreply.github.com>
2022-04-22 10:46:52 +0100

    Deploy contract

---
+++ Udemy2022/Project 2 - The Auction Contract/Deploy.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.11;
+
+contract A{
+
+	address public ownerA;
+
+	constructor(){
+		ownerA = msg.sender;
+	}
+}
+
+contract Creator{
+	address public 	ownerCreator;
+	A[] public deployedA;
+
+	constructor(){
+		ownerCreator = msg.sender;
+	}
+
+	function deployA() public{
+		A new_A_address = new A();
+		deployedA.push(new_A_address);
+	}
+}
